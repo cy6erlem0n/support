e816617485d862214544ab290bc7d4dde51d9051
cy6erlem0n <dmytriy.drozd.2014@gmail.com>
2024-07-07 22:05:52 +0200

    Make a new test

---
+++ Test/project/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div> 
+        <div>
+            <button v-on:click="addLike">Like</button>
+            <button @click="addDislike">Disike</button>
+        </div>
+        <div>Count of likes: <strong>{{ likes }}</strong></div>
+        <div>Count of dislikes: <strong>{{ dislikes }}</strong></div> 
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            likes: 0,
+            dislikes: 0,
+        }
+    },
+    methods: {
+        addLike() {
+            this.likes += 1;
+        },
+        addDislike() {
+            this.dislikes += 1;
+        }
+    }
+}
+</script>
+
+
+<style>
+
+</style>
